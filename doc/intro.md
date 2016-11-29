877612423e2262d96df7d11194d9faca6e7ec155
alex gherega <alex.gherega@gmail.com>
2016-11-29 09:45:27 +0200

    new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bike-point
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bike_point/core_test.clj
@@ -0,0 +1,7 @@
+(ns bike-point.core-test
+  (:require [clojure.test :refer :all]
+            [bike-point.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
